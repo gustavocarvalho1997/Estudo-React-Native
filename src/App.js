9f61d684e9916915da4c3e09a782d8c243c85445
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2024-03-04 15:49:22 -0300

    Inicio do uso do Text

---
+++ src/App.js
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {Text} from 'react-native';
+
+function App() {
+    return <Text>Hello, world!</Text>;
+}
+
+export default App;
