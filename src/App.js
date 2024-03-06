8cf2ecdc17c8eeaca365223ca269c257717cb8b8
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2024-03-06 13:34:04 -0300

    Exemplo de exportar mais de um componente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import {View} from 'react-native';
+
+import Comp, {Comp1, Comp2} from './components/Multi';
 import Primeiro from './components/Primeiro';
 
 export default () => (
     <View>
+        <Comp />
+        <Comp1 />
+        <Comp2 />
         <Primeiro />
     </View>
 );
