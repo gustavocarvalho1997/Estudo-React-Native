3123813bd5ae4eea61a27eb143245f369f06cdc8
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2024-03-06 13:57:00 -0300

    testando o console.warn

--- src/components/Primeiro.js
+++ src/components/Primeiro.js
@@ -3,4 +3,9 @@ import React from 'react';
 import {Text} from 'react-native';
 import Estilo from './estilo';
 
-export default () => <Text style={Estilo.TextoGrande}>Primeiro Componente</Text>;
+export default () => {
+    // console.warn('Primeiro');
+    return (
+        <Text style={Estilo.TextoGrande}>Primeiro Componente!</Text>
+    );
+};
