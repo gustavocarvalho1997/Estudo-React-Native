578b331a87bfabe0478be804da0e86ec112451b2
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2024-03-07 16:54:01 -0300

    uso de fragments

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
-import MinMax from './components/MinMax';
-import Aleatorio from './components/Aleatorio';
+import Titulo from './components/Titulo';
+// import MinMax from './components/MinMax';
+// import Aleatorio from './components/Aleatorio';
 // import Comp, {Comp1, Comp2} from './components/Multi';
 // import Primeiro from './components/Primeiro';
 
 export default () => (
     <View style={style.App}>
-        <Aleatorio min={1} max={60} />
+        <Titulo principal="Cadastro Produto" secundario="Tela de Cadastro do Produto" />
+        {/* <Aleatorio min={1} max={60} /> */}
         {/* <MinMax min={3} max={20}/> */}
         {/* <Comp />
         <Comp1 />
---
+++ src/components/Titulo.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View, Text} from 'react-native';
+import Estilo from './estilo';
+
+export default props => {
+  return (
+    <>
+      <Text style={Estilo.TextoGrande}>{props.principal}</Text>
+      <Text>{props.secundario}</Text>
+    </>
+  );
+}
